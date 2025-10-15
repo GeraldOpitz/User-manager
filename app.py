81781b5166337a164c1cfb1e2b4e801d73c5c9f9
mvalladares2025 <mvalladares@edutecno.com>
2025-10-15 19:49:40 -0300

    FAUM-6 - Add user funcionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,6 +18,20 @@ class User(db.Model):
 def index():
     users = User.query.all()
     return render_template('index.html', users=users)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        role = request.form['role']
+
+        new_user = User(name=name, email=email, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('add_user.html')
 
 if __name__ == '__main__':
     with app.app_context():
