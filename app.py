870ee858d93964a49e61b3a1177b7fcce576d59d
mvalladares2025 <mvalladares@edutecno.com>
2025-10-15 19:37:04 -0300

    FAUM-5 - Connecction DB

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,7 +14,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     role = db.Column(db.String(10), nullable=False)
 
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database and tables created successfully.")
+    app.run(debug=True)
