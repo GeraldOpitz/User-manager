dd30d642fc84bd02c1b9cbe1809de8b257f9b9ef
GeraldOpitz <geraldopitz@gmail.com>
2025-10-15 18:54:20 -0300

    FAUM-3 User model and database created

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Database and tables created successfully.")
